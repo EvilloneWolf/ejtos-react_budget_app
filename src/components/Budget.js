7ddd2b842a78c21095002eaeec958aaab9395958
Ayush Yadav <yayush067@gmail.com>
2023-08-11 11:47:42 +0530

    Final changes to the file
    React js project completed!!!

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,67 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch,currency  } = useContext(AppContext);
+    const [updatedBudget, setUpdatedBudget] = useState(budget);
+
+    const totalAllocatedSpending = expenses.reduce((total, item) => total + item.cost, 0);
+    const lowerLimit = totalAllocatedSpending;
+
+    const handleBudgetChange = (event) => {
+        const newValue = parseFloat(event.target.value);
+        if (newValue >= lowerLimit && newValue <= 20000) {
+            setUpdatedBudget(newValue);
+        } else if (newValue < lowerLimit) {
+            alert('Budget cannot be lower than allocated spending');
+        } else {
+            alert('Budget cannot exceed 20,000');
+        }
+    };
+
+    const handleIncrease = () => {
+        const newValue = updatedBudget + 10;
+        if (newValue <= 20000) {
+            setUpdatedBudget(newValue);
+        } else {
+            alert('Budget cannot exceed 20,000');
+        }
+    };
+
+    const handleDecrease = () => {
+        const newValue = updatedBudget - 10;
+        if (newValue >= lowerLimit) {
+            setUpdatedBudget(newValue);
+        } else {
+            alert('Budget cannot be lower than allocated spending');
+        }
+    };
+
+    const handleSave = () => {
+        if (updatedBudget >= lowerLimit && updatedBudget <= 20000) {
+            dispatch({ type: 'SET_BUDGET', payload: updatedBudget });
+        } else if (updatedBudget < lowerLimit) {
+            alert('Budget cannot be lower than allocated spending');
+        } else {
+            alert('Budget cannot exceed 20,000');
+        }
+    };
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <div>
+                <span>Budget: {currency}</span>
+                <input
+                    type='number'
+                    value={updatedBudget}
+                    onChange={handleBudgetChange}
+                    onBlur={handleSave}
+                />
+                <button onClick={handleIncrease}>+10</button>
+                <button onClick={handleDecrease}>-10</button>
+            </div>
         </div>
     );
 };
+
 export default Budget;
